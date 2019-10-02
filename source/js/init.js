f7bc60051e29e88124e6dfd2d825338d8cdb1e5d
adam bezemek <adam.bezemek@xcentium.com>
2019-10-02 13:05:39 -0500

    comment out jquery by default

--- source/js/init.js
+++ source/js/init.js
@@ -1,12 +1,13 @@
 console.log('init');
 
-import $ from 'jquery';
+//UNCOMMENT THIS IF YOU WANT TO USE JQUERY
+// import $ from 'jquery';
 
-//Better jQuery = https://learn.jquery.com/code-organization/beware-anonymous-functions/
-var APP = {
-    onReady: function() {
-        console.log('ready!');
-    },
-};
+// // Better jQuery = https://learn.jquery.com/code-organization/beware-anonymous-functions/
+// var APP = {
+//     onReady: function() {
+//         console.log('ready!');
+//     },
+// };
 
-$(document).ready(APP.onReady);
+// $(document).ready(APP.onReady);
