abb7df9632bfb1511696365900c0577f87556d24
adam bezemek <adam.bezemek@xcentium.com>
2019-10-28 12:33:19 -0500

    patternlab updates

--- webpack.config.js
+++ webpack.config.js
@@ -220,6 +220,18 @@ module.exports = env => {
                         'sass-loader',
                     ],
                 },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/',
+                            },
+                        },
+                    ],
+                },
             ],
         },
     });
